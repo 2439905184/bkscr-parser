d50ce2904a2dbe1cf9f27007359cdb2df9fb01dd
2439905184 <suchenglong018@126.com>
2022-09-30 22:15:22 +0800

    🦄 refactor: 重构一下

--- 基本单元 例子/单行宏 词法分析器.py
+++ 基本单元 例子/单行宏 词法分析器.py
@@ -1,76 +1,120 @@
-a = "@sprite index=0 file='a.png' rect=[0,0,0,0] "
-start = 0 #开始位置 为0
-current = 0 #当前位置 
+#a = '@sprite index=7768 file="a.png" rect=[0,0,0,0]'
+#a = '@bg aaaaf="ajoig" br=[0,2]'
 
-tokens = []
-def eatNext()->str:
-    global current
-    current += 1
-    return a[current -1]
-# 取字符串的值
-def getMacro():
-    global current
-    global start
-    # 原理就是 "  " 作为字符串两端，获得这两个符号的位置，并提取子字符串
-    while peek() != ' ' and not isAtEnd():
-        current += 1
-    value = a[start +1 : current]
-    #print(value)
-    tokens.append(value)
-    pass
+class parser:
+    tokens = []
+    source = ""
+    start = 0 # 开始位置
+    current = 0 # 当前位置 
 
-def getParamName():
-    global current
-    global start
-    while peek() != "=" and not isAtEnd():
-        eatNext()
-    if isAtEnd():
-        print("参数名不完整")
-        return
-    value:str = a[start : current]
-    #print(value)
-    tokens.append(value)
+    def __init__(self, source):
+        self.source = source
+        
+    # 扫描标记
+    def scanTokens(self, source)->list:
+        while not self.self.isAteEnd()():
+            self.start = self.current
+            #print("正在遍历每个字符：",start)
+            self.scanToken(source)
+        return self.tokens
 
-def getParamValue():
-    global current
-    global start
-    while peek() != " " and not isAtEnd():
-        eatNext()
-    if isAtEnd():
-        print("参数名不完整")
-        return
-    value:str = a[start+1 : current]
-    #print(value)
-    tokens.append(value)
-# 向后读取字符数据
-def peek():
-    if isAtEnd(): return "\0"
-    return a[current]
+    def eatNext(self, source)->str:
+        self.current += 1
+        return source[self.current -1]
 
-def isAtEnd():
-    # 如果 >= len(a) 返回 true 否则返回false
-    return current >= len(a)
-
-def scanToken():
-    c = eatNext()
-    if c == "@":
-        tokens.append("单行宏")
-        getMacro()
-    elif c == " ": pass
-    elif c == "=":
-        getParamValue()
-    else:
-        getParamName()
+    # 取字符串的值
+    def getMacro(self, source):
+        # 原理就是 "  " 作为字符串两端，获得这两个符号的位置，并提取子字符串
+        while self.peek() != ' ' and not self.self.isAteEnd()():
+            self.eatNext(source)
+        if self.isAteEnd()():
+            print("第"+ str(self.current) + "列处,宏名不完整")
+            return
+        value = a[self.start +1 : self.current]
+        #print(value)
+        self.tokens.append(value)
         pass
-# 扫描标记
-def scanTokens():
-    global start
-    global current
-    #print("start:")
-    while not isAtEnd():
-        start = current
-        #print("正在遍历每个字符：",start)
-        scanToken()
-    return tokens
-result = scanTokens()
-print(result)
+    
+    def getParamName(self):
+        global current
+        global start
+        while self.peek() != "=" and not self.isAteEnd()():
+            self.eatNext()
+        if self.isAteEnd()():
+            print("第"+ str(current) + "列处,参数名不完整")
+            return
+        value:str = a[start : current]
+        print("参数名称",value)
+        self.tokens.append(value)
+
+    def getInt(self):
+        while self.peek() != ' ' and not self.isAteEnd()():
+            self.eatNext()
+        if self.isAteEnd()():
+            print("第" + str(self.current) + "列处，int参数代码不完整")
+            return
+        value:str = a[self.start : self.current]
+        # print("int型的值为",value)
+        self.tokens.append(value)
+
+    def getString(self):
+        while self.peek() != '"' and not self.self.isAteEnd()():
+            self.eatNext()
+        if self.self.isAteEnd()():
+            print("第" + str(current) + "列处，字符串代码不完整")
+            return
+        value:str = a[start+1 : current]
+        # print("string型的值为",value)
+        self.tokens.append(value)
+
+    def getArray(self):
+        while self.peek() != "]" and not self.self.isAteEnd()():
+            self.eatNext()
+        if self.self.isAteEnd()():
+            print("第" + str(self.current) + "列处，数组代码不完整")
+            return
+        value:str = a[self.start+2 : self.current]
+        print("数组型的值为",value)
+        ar = value.split(",")
+        for v in ar:
+            self.tokens.append(value)
+
+    # 向后读取字符数据
+    def peek(self):
+        if self.self.isAteEnd()(): return "\0"
+        return self.source[self.current]
+
+    def isAteEnd(self):
+        # 如果 >= len(a) 返回 true 否则返回false
+        return self.current >= len(self.source)
+
+    def scanToken(self, source):
+        c = self.eatNext(source)
+        if c == "@":
+            self.getMacro()
+        elif c == " ": pass
+        elif c.isalpha():
+            print("是字母",c)
+            self.getParamName()
+        elif c == "=":pass
+        elif c.isdigit():
+            self.getInt()
+        elif c == '"':
+            self.getString()
+        # elif c == "[":
+        #     getArray()
+ff = open("../test/单行.bkscr","r",encoding="utf-8")
+source = ff.read()
+print(source)
+ff.close()
+parser("A")
+#parserObject = parser(source)
+#parserObject.scanTokens(parserObject.source)
+
+# result = scanTokens(a)
+# print(result)
+# file = open("out.txt","w",encoding="utf-8")
+# for element in result:
+#     #print(element)
+#     file.write(element + "\n")
+# file.close()
