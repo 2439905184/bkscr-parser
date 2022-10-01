a39b5371462b92b7978f047ac7a01cacc527bbc2
2439905184 <suchenglong018@126.com>
2022-10-01 20:07:30 +0800

    同步

--- main.py
+++ main.py
@@ -1,38 +1,12 @@
-"""
-def 多行宏解析单元():
-    labels = []
-    current_line_type = ""
-    file = open("test/main.bkscr","r",encoding="utf-8")
-    for line in file.readlines():
-        print(line)
-        for char in line:
-            if char == "*":
-                current_line_type = "label"
-                break
-            if char == "[" or char == "]":
-                current_line_type = "mulit_macro"
-                break
-        if current_line_type == "label":
-            current_label = line
-            labels.append(current_label)
-        if current_line_type == "mulit_macro":
+import SingleMacroParser as sp
+import MulitMacroParser as mp
 
-    
-多行宏解析单元() """
-
-import scanner
-
-def run(source):
-    bkscrScanner = scanner.Scanner(source)
-    tokens = bkscrScanner.scanTokens()
-    #print(bkscrScanner)
-    print(tokens)
-    #for token in tokens:
-        # print(token)
-def runFile():
-    file = open("test/main.bkscr","r",encoding="utf-8")
-    content = file.read()
-    #print(content)
-    run(content)
-    file.close()
-runFile()
+#mp.parse("")
+file1 = open("test/单行.bkscr","r",encoding="utf-8-sig")
+lines = file1.readlines()
+#print(lines)
+src = "".join(lines)
+#print(src)
+result = sp.parse(src)
+print(result)
+file1.close()
