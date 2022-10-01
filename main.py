ee88f7d5a4d338a1b58b0c90ec4547c4e46f2e31
2439905184 <suchenglong018@126.com>
2022-10-01 21:13:56 +0800

    ✨ feat: 添加编译输出

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import SingleMacroParser as sp
 import MulitMacroParser as mp
 
 #mp.parse("")
-# 注意 此处默认使用utf-8 with bom 编码打开，因为bkengine的脚本编码默认就是这种的
-file1 = open("test/单行.bkscr","r",encoding="utf-8-sig")
+# 注意 此处默认使用utf-8 with bom 编码打开，因为bkengine的脚本编码默认就是这种的，麻了，直接全部转utf-8格式
+file1 = open("test/单行.bkscr","r",encoding="utf-8")
 lines = file1.readlines()
 print("所有行")
 print(lines)
@@ -21,5 +21,9 @@ for line in lines:
 # result = sp.parse(src)
 print("结果")
 print(result)
-#open("compile_out/" + file_name + ".bkscr","r",encoding="utf-8-sig")
+file_name = "out.txt"
+out = open("compile_out/" + file_name,"w",encoding="utf-8")
+for element in result:
+    out.write(element + "\n")
+out.close()
 file1.close()
