576cc03411c08390ba385908bcb2279f013aba7b
2439905184 <suchenglong018@126.com>
2022-10-01 15:35:35 +0800

    🎉 init: 正式创建单行宏解析器

--- SingleMacroParser.py
+++ SingleMacroParser.py
@@ -1,65 +1,4 @@
-def 获得参数名1():
-    a = "@bg abc=1"
-    split_start = 0
-    split_end = 0
-    start = 0
-    current = 0
-    equals_index = 0
-    tokens = []
-    # 用于归位的索引
-    priv_index = 0
-
-    def isAtStart():
-        if current < start:
-            return True
-        else:
-            return False
-
-    def isAtEnd():
-        nonlocal current
-        if current >= len(a):
-            return True
-        else: 
-            return False
-
-    def readBack():
-        nonlocal current
-        current -= 1
-        return a[current -1]
-
-    def readNext():
-        nonlocal current
-        current += 1
-        return a[current -1]
-
-    def peekBack():
-        nonlocal current
-        return a[current-1]
-
-    while not isAtEnd(): 
-        c = readNext()
-        #print(c)
-        if c == "=":
-            equals_index = current -1
-            priv_index = current
-            while peekBack() != " " and not isAtStart():
-                readBack()
-            if isAtStart():
-                print("越界")
-                break
-            split_start = equals_index - len(a)
-            split_end = current - len(a)
-            revert_str = a[split_start - 1 : split_end -1 :-1]
-            s = revert_str[::-1]
-            tokens.append(s)
-            # 索引归位
-            current = priv_index
-    # 开始位置是实际位置-1 结束位置是实际位置
-
-    print(tokens)
-
-def 分析标记():
-    code = "@addto index=16568 pos=[0,0] //这是注释comment"
+def parse(code:str)->list:
     split_start = 0
     split_end = 0
     start = 0
@@ -172,5 +111,4 @@ def 分析标记():
                 print("注释代码编写错误,列: ",current)
                 break
     print(tokens)
-#获得参数名1()
-分析标记()
+    return tokens
