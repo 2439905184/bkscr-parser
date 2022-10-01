7411d265d082e5c9edc410512ffb7c60964994bf
2439905184 <suchenglong018@126.com>
2022-10-01 22:43:12 +0800

    ✨ feat: 添加命令行传参

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import os
+import sys
 import SingleMacroParser as sp
 import MulitMacroParser as mp
 
+arg = sys.argv[1]
+print("输入文件",arg)
+filein = os.path.split(arg)[1]
 # 注意 此处默认使用utf-8 with bom 编码打开，因为bkengine的脚本编码默认就是这种的，麻了，直接全部转utf-8格式
-srcfile = open("test/多行.bkscr","r",encoding="utf-8")
+srcfile = open(arg,"r",encoding="utf-8")
 lines = srcfile.readlines()
 print("所有行")
 print(lines)
@@ -10,8 +15,10 @@ srcfile.close()
 
 result = []
 
-file_name = "out.txt"
-out = open("compile_out/" + file_name,"w",encoding="utf-8")
+fileout = filein.replace(".bkscr",".txt")
+print("输出文件",fileout)
+# file_name = arg
+out = open("compile_out/" + fileout,"w",encoding="utf-8")
 for line in lines:
     buffer_line = ""
     if line[0] == "@":
