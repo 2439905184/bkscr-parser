be77ad91680873c2d1f8a3fcf06c0e54a7431d8c
2439905184 <suchenglong018@126.com>
2022-10-01 22:27:13 +0800

    ✨ feat: 完善main函数

---
+++ MulitMacroParser.py
@@ -0,0 +1,15 @@
+# 基于正则表达
+import re
+def parse(code:str)->list:
+    #匹配字母 下划线 数字 或者字符串里面的内容
+    regex = r"([a-z_0-9]+)|(\"(.*)\")"
+
+    #test_str = '[sprite index=0 file="a.png" rect=[0,0,0,0] ]'
+    pre = re.sub(r"//[^\n]*"," ",code)
+    #print("预处理",pre)
+    matches = re.finditer(regex, pre)
+    result = []
+    for matchNum, match in enumerate(matches, start=1):
+        m = match.group().strip('"')
+        result.append(m)
+    return result
--- main.py
+++ main.py
@@ -1,29 +1,28 @@
 import SingleMacroParser as sp
 import MulitMacroParser as mp
 
-#mp.parse("")
 # 注意 此处默认使用utf-8 with bom 编码打开，因为bkengine的脚本编码默认就是这种的，麻了，直接全部转utf-8格式
-file1 = open("test/单行.bkscr","r",encoding="utf-8")
-lines = file1.readlines()
+srcfile = open("test/多行.bkscr","r",encoding="utf-8")
+lines = srcfile.readlines()
 print("所有行")
 print(lines)
-
+srcfile.close()
 
 result = []
-for line in lines:
-    if line[0] == "@":
-        result = sp.parse(line)
-        break
-    # if line[0] == "[":
-    #     pass
-#src = "".join(lines)
-# print(src)
-# result = sp.parse(src)
-print("结果")
-print(result)
+
 file_name = "out.txt"
 out = open("compile_out/" + file_name,"w",encoding="utf-8")
+for line in lines:
+    buffer_line = ""
+    if line[0] == "@":
+        buffer_line = "\n".join(sp.parse(line))
+        #print("缓冲区",buffer_line)
+        result.append(buffer_line)
+    if line[0] == "[":
+        buffer_line = "\n".join(mp.parse(line))
+        #print("缓冲区",buffer_line)
+        result.append(buffer_line)
+print("结果",result)
 for element in result:
     out.write(element + "\n")
 out.close()
-file1.close()
