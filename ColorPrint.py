c376f912569fcaadfd254bb618a7761d4c614c5c
2439905184 <suchenglong018@126.com>
2022-10-03 20:25:32 +0800

    添加函数

--- ColorPrint.py
+++ ColorPrint.py
@@ -20,18 +20,22 @@ def print_compile(*args):
     print(out)
 
 # 仅输出一次详细输出提示，在开始所有详细输出之前调用一次
-def print_verbose_hint():
+def print_verbose_once():
     print("\033[1;31;40m详细输出\033[0m")
 
 # 仅输出一次编译开始提示，在所有编译开始之前调用一次
-def print_compile_hint():
-    print("\033[1;31;40m开始编译\033[0m")
+def print_compile_once(arg):
+    print("\033[1;31;40m" + arg + "\033[0m")
+
+# 可重复使用的函数
+def print_verbose_hint(arg):
+    print("\033[1;31;40m" + arg + "开始编译\033[0m")
 
 def test():        
     print_verbose_hint()
     for i in range(20):
         print_verbose("@init","提示")
-    print_compile_hint()
+    print_compile_once()
     for i in range(20):
         print_compile("@sprite","提示")
 
