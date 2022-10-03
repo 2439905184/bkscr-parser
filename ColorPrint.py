a57c4263ceaf86b31f1c6019066d4f28869b81af
2439905184 <suchenglong018@126.com>
2022-10-03 17:19:36 +0800

    ✨ feat: 添加带颜色的输出模块
    用于美化输出提示，方便快速查找到编译器的问题

---
+++ ColorPrint.py
@@ -0,0 +1,38 @@
+"""终端的字符颜色是用转义序列控制的，是文本模式下的系统显示功能，和具体的语言无关。
+   转义序列是以ESC开头,即用\033来完成（ESC的ASCII码用十进制表示是27，用八进制表示就是033）。
+"""
+def print_verbose(*args):
+    '''
+    :args: 可变参数[0]是代码行，[1]是输出提示
+    '''
+    to = "\033[0;33;40m" + args[0] + " --- " + args[1] + "\033[0m"
+    print(to)
+
+def print_compile(*args):
+    '''
+    :args: 可变参数[0]是代码行，[1]是输出提示
+    '''
+    #to = args[0] + "\033[0;32;40m"  + " -> " + "\033[0m" + args[1]
+    source_in = "\033[0;34;40m@bg\033[0m"
+    arrow = "->"
+    compile_out = "\033[0;32;40m"+ args[1] + "\033[0m"
+    out = source_in + " " + arrow + " " + compile_out
+    print(out)
+
+# 仅输出一次详细输出提示，在开始所有详细输出之前调用一次
+def print_verbose_hint():
+    print("\033[0;41;40m详细输出\033[0m")
+
+# 仅输出一次编译开始提示，在所有编译开始之前调用一次
+def print_compile_hint():
+    print("\033[0;41;40m开始编译\033[0m")
+
+def test():        
+    print_verbose_hint()
+    for i in range(20):
+        print_verbose("@bg","提示")
+    print_compile_hint()
+    for i in range(20):
+        print_compile("@bg","提示")
+
+test()
