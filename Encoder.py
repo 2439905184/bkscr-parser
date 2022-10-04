db433c15c19f32ad17b6e3bb6bf069e6944c10a2
2439905184 <suchenglong018@126.com>
2022-10-04 10:25:54 +0800

    🦄 refactor: 添加是否显示详细输出的参数
    适用于性能优化

--- Encoder.py
+++ Encoder.py
@@ -3,8 +3,9 @@ import os
 from chardet import detect
 import ColorPrint
 
-def encodeProject(dir:str) -> bool:
-    ColorPrint.print_verbose_hint("开始转换工程的编码格式")
+def encodeProject(dir:str, verbose: bool) -> bool:
+    if verbose:
+        ColorPrint.print_verbose_hint("开始转换工程的编码格式")
     L = []
     for root, dirs, files in os.walk(dir):
         #获得所有bkscr文件
@@ -18,12 +19,14 @@ def encodeProject(dir:str) -> bool:
             with open(path, 'rb+') as fp:
                 old_content = fp.read()
                 encoding = detect(old_content)['encoding']
-                ColorPrint.print_verbose_hint("检测到编码 -> " + encoding)
+                if verbose:
+                    ColorPrint.print_verbose_hint("检测到编码 -> " + encoding)
                 if encoding == "UTF-8-SIG":
                     content = old_content.decode(encoding).encode('utf8')
                     fp.seek(0)
                     fp.write(content)
-                    ColorPrint.print_compile(old_content.decode("utf-8-sig"),content.decode("utf-8"))
+                    if verbose:
+                        ColorPrint.print_compile(old_content.decode("utf-8-sig"),content.decode("utf-8"))
                 elif encoding == "utf-8" or encoding == "ascii": pass
                 else:
                     ColorPrint.print_verbose_hint("暂时不支持转换的编码格式,或者你需要手动修改转换器代码并添加新的编码格式转换支持")
